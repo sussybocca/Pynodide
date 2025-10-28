02dd224310fe3c0ac0a11c920097b4a091f3c7ba
sussybocca <babyyodacutefry@gmail.com>
2025-10-28 07:50:19 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,1 +1,14 @@
+import Link from "next/link";
 
+export default function Navbar() {
+  return (
+    <header className="border-b bg-zinc-900 text-white flex items-center justify-between p-4">
+      <h1 className="text-lg font-bold">MyHost</h1>
+      <nav className="space-x-4">
+        <Link href="/">Editor</Link>
+        <Link href="/explore">Explore</Link>
+        <Link href="/profile/me">Profile</Link>
+      </nav>
+    </header>
+  );
+}
