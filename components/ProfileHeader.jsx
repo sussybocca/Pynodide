d471bf8c3a92c179a70438b55eca7f73685c1160
sussybocca <babyyodacutefry@gmail.com>
2025-10-28 07:48:32 -0500

    Update ProfileHeader.jsx

--- components/ProfileHeader.jsx
+++ components/ProfileHeader.jsx
@@ -1,1 +1,8 @@
-
+export default function ProfileHeader({ name, github }) {
+  return (
+    <header style={{ marginBottom: 20 }}>
+      <h1>{name}</h1>
+      <p>GitHub: <a href={`https://github.com/${github}`}>{github}</a></p>
+    </header>
+  );
+}
