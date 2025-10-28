92419e7dadccce219be5f078b0a8ffe1574b6c35
sussybocca <babyyodacutefry@gmail.com>
2025-10-28 07:38:32 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    SMTP_HOST: process.env.SMTP_HOST,
+    SMTP_PORT: process.env.SMTP_PORT,
+    SMTP_USER: process.env.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    BASE_URL: process.env.BASE_URL,
+    GITHUB_PAT: process.env.GITHUB_PAT
+  }
+};
+module.exports = nextConfig;
