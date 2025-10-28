c91969a65d612ef64799286e5ef81d32f3f7fc2e
sussybocca <babyyodacutefry@gmail.com>
2025-10-28 07:48:08 -0500

    Update ProfileCard.jsx

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,1 +1,14 @@
-
+export default function ProjectCard({ project }) {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: 10, margin: 10 }}>
+      <h3>{project.name}</h3>
+      <a href={project.github_pages_url} target="_blank">
+        Visit Site
+      </a>
+      <br />
+      <a href={project.github_repo} target="_blank">
+        GitHub Repo
+      </a>
+    </div>
+  );
+}
